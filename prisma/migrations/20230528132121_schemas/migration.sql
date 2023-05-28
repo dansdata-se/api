c117c7f39f3d214d32a9f90d7bd7171aafeaf9ee
Felix Zedén Yverås <felix@fzy.se>
2023-05-28 12:13:28 +0000

    feat: first version of `profiles` database schema

---
+++ prisma/migrations/20230528132121_schemas/migration.sql
@@ -0,0 +1,14 @@
+-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "auth";
+
+-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "events";
+
+-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "portal";
+
+-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "profiles";
+
+-- CreateSchema
+CREATE SCHEMA IF NOT EXISTS "storage";
