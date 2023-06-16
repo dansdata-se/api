8b7041bfd89b37c9647662aa3573f7b240595271
Felix Zedén Yverås <felix@fzy.se>
2023-06-16 20:42:07 +0000

    feat: add `BaseProfileDAO`
    This DAO will be used by e.g. `VenueDAO` and `OrganizationDAO` to
    retrieve common information from the base `profiles` table.
    
    Currently, only simple get by id is implemented.

---
+++ src/db/dao/profiles/base_profile.test.ts
@@ -0,0 +1,178 @@
+/**
+ * @group unit
+ */
+
+import {
+  ImageEntity,
+  ImageVariant,
+  PrismaClient,
+  ProfileEntity,
+  ProfileLinkEntity,
+  ProfileType,
+} from "@prisma/client";
+import { DeepMockProxy, mockDeep, mockReset } from "jest-mock-extended";
+
+jest.mock("@/db", () => ({
+  __esModule: true,
+  prisma: mockDeep<PrismaClient>(),
+}));
+// prevent prettier from moving this import around
+// prettier-ignore
+import { prisma } from "@/db";
+
+import { BaseProfileDAO } from "@/db/dao/profiles/base_profile";
+import { BaseProfileModel } from "@/model/profiles/profile";
+import { BaseProfileReferenceModel } from "@/model/profiles/profile_reference";
+
+describe("BaseProfileDAO unit tests", () => {
+  const prismaMock = prisma as unknown as DeepMockProxy<PrismaClient>;
+
+  beforeEach(() => {
+    mockReset(prismaMock);
+  });
+
+  test("getById", async () => {
+    const expectedProfileEntity: ProfileEntity & {
+      images: {
+        image: ImageEntity;
+      }[];
+      links: ProfileLinkEntity[];
+    } = {
+      id: "profileId",
+      type: ProfileType.organization,
+      name: "Profile Name",
+      description: "Profile description",
+      links: [
+        {
+          id: 1,
+          profileId: "profileId",
+          url: "https://profile.example",
+        },
+        {
+          id: 2,
+          profileId: "profileId",
+          url: "https://facebooke.com/profile",
+        },
+      ],
+      images: [
+        {
+          image: {
+            id: "coverImageId",
+            cloudflareId: "coverImageCloudflareId",
+            variant: ImageVariant.cover,
+          },
+        },
+        {
+          image: {
+            id: "posterImageId",
+            cloudflareId: "posterImageCloudflareId",
+            variant: ImageVariant.poster,
+          },
+        },
+        {
+          image: {
+            id: "squareImageId",
+            cloudflareId: "squareImageCloudflareId",
+            variant: ImageVariant.square,
+          },
+        },
+      ],
+      createdAt: new Date("2023-06-16T22:26:49"),
+    };
+    prismaMock.profileEntity.findUnique.mockResolvedValueOnce(
+      expectedProfileEntity
+    );
+
+    await expect(
+      BaseProfileDAO.getById("profileId")
+    ).resolves.toEqual<BaseProfileModel>({
+      id: expectedProfileEntity.id,
+      type: expectedProfileEntity.type,
+      name: expectedProfileEntity.name,
+      description: expectedProfileEntity.description,
+      links: expectedProfileEntity.links.map((l) => ({ url: l.url })),
+      images: {
+        cover: {
+          id: "coverImageId",
+          cloudflareId: "coverImageCloudflareId",
+          variant: ImageVariant.cover,
+        },
+        poster: {
+          id: "posterImageId",
+          cloudflareId: "posterImageCloudflareId",
+          variant: ImageVariant.poster,
+        },
+        square: {
+          id: "squareImageId",
+          cloudflareId: "squareImageCloudflareId",
+          variant: ImageVariant.square,
+        },
+      },
+    });
+  });
+
+  test("getReferenceById", async () => {
+    const expectedProfileEntity: ProfileEntity & {
+      images: {
+        image: ImageEntity;
+      }[];
+    } = {
+      id: "profileId",
+      type: ProfileType.organization,
+      name: "Profile Name",
+      description: "Profile description",
+      images: [
+        {
+          image: {
+            id: "coverImageId",
+            cloudflareId: "coverImageCloudflareId",
+            variant: ImageVariant.cover,
+          },
+        },
+        {
+          image: {
+            id: "posterImageId",
+            cloudflareId: "posterImageCloudflareId",
+            variant: ImageVariant.poster,
+          },
+        },
+        {
+          image: {
+            id: "squareImageId",
+            cloudflareId: "squareImageCloudflareId",
+            variant: ImageVariant.square,
+          },
+        },
+      ],
+      createdAt: new Date("2023-06-16T22:26:49"),
+    };
+    prismaMock.profileEntity.findUnique.mockResolvedValueOnce(
+      expectedProfileEntity
+    );
+
+    await expect(
+      BaseProfileDAO.getReferenceById("profileId")
+    ).resolves.toEqual<BaseProfileReferenceModel>({
+      id: expectedProfileEntity.id,
+      type: expectedProfileEntity.type,
+      name: expectedProfileEntity.name,
+      images: {
+        cover: {
+          id: "coverImageId",
+          cloudflareId: "coverImageCloudflareId",
+          variant: ImageVariant.cover,
+        },
+        poster: {
+          id: "posterImageId",
+          cloudflareId: "posterImageCloudflareId",
+          variant: ImageVariant.poster,
+        },
+        square: {
+          id: "squareImageId",
+          cloudflareId: "squareImageCloudflareId",
+          variant: ImageVariant.square,
+        },
+      },
+    });
+  });
+});
---
+++ src/db/dao/profiles/base_profile.ts
@@ -0,0 +1,72 @@
+import { prisma } from "@/db";
+import { imageEntitiesToImagesModel } from "@/db/dao/storage/image";
+import { BaseProfileModel } from "@/model/profiles/profile";
+import { BaseProfileReferenceModel } from "@/model/profiles/profile_reference";
+
+/**
+ * DAO for working with profiles based on their common fields
+ */
+export const BaseProfileDAO = {
+  /**
+   * Retrieve a full profile by its id
+   */
+  async getById(id: BaseProfileModel["id"]): Promise<BaseProfileModel | null> {
+    const entity = await prisma.profileEntity.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        links: true,
+        images: {
+          select: {
+            image: true,
+          },
+        },
+      },
+    });
+    if (entity === null) return null;
+
+    return {
+      id: entity.id,
+      type: entity.type,
+      name: entity.name,
+      description: entity.description,
+      links: entity.links.map((l) => ({ url: l.url })),
+      images: imageEntitiesToImagesModel(entity.images.map((it) => it.image)),
+    };
+  },
+  /**
+   * Retrieve a profile reference by its id
+   *
+   * Profile references are used when we need to refer to a profile without this
+   * reference including further references to other profiles and so forth.
+   *
+   * Profile references typically contain just enough data for a client to
+   * render a nice looking link for end users without having to look up the full
+   * profile first.
+   */
+  async getReferenceById(
+    id: BaseProfileModel["id"]
+  ): Promise<BaseProfileReferenceModel | null> {
+    const entity = await prisma.profileEntity.findUnique({
+      where: {
+        id,
+      },
+      include: {
+        images: {
+          select: {
+            image: true,
+          },
+        },
+      },
+    });
+    if (entity === null) return null;
+
+    return {
+      id: entity.id,
+      type: entity.type,
+      name: entity.name,
+      images: imageEntitiesToImagesModel(entity.images.map((it) => it.image)),
+    };
+  },
+};
