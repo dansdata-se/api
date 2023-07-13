b779856bb8387ad02cee39cf3d8ddd15a2531af7
Felix Zedén Yverås <felix@fzy.se>
2023-07-13 21:31:52 +0000

    build: upgrade eslint and remove safeql
    safeql caused the linter to freeze up completely in new code. `ctrl-c`
    was not able to break out of this. I was unable to find a workaround.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,18 @@
 module.exports = {
-  extends: ["next/core-web-vitals", "prettier"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "@ts-safeql/eslint-plugin"],
-  overrides: [
-    {
-      files: ["*.ts", "*.tsx"],
-      extends: [
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
-        "plugin:@typescript-eslint/strict",
-        "next/core-web-vitals",
-        "prettier",
-      ],
-      parserOptions: {
-        project: ["./tsconfig.json"],
-      },
-    },
+  extends: [
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:@typescript-eslint/strict",
+    "next/core-web-vitals",
+    "prettier",
   ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  root: true,
   rules: {
     eqeqeq: "error",
     "no-restricted-imports": [
@@ -29,19 +25,6 @@ module.exports = {
             importNames: ["default", "z"],
             message:
               'Use `import z from "@/api/zod"` instead for consistent extensions.',
-          },
-        ],
-      },
-    ],
-    "@ts-safeql/check-sql": [
-      "error",
-      {
-        connections: [
-          {
-            migrationsDir: "./prisma/migrations",
-            targets: [
-              { tag: "prisma.+($queryRaw|$executeRaw)", transform: "{type}[]" },
-            ],
           },
         ],
       },
