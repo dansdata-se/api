8391ed708533a0e77d61ca31116f3b726691410a
Felix Zedén Yverås <felix@fzy.se>
2023-07-31 20:58:44 +0000

    test: make sure (integration)tests run serially
    Otherwise, integration tests using the same database tables may interact
    adversely with each other.

--- jest.config.mjs
+++ jest.config.mjs
@@ -9,6 +9,8 @@ const config = {
   setupFilesAfterEnv: ["<rootDir>/src/__test__/jest.setup.ts"],
   testEnvironment: "jest-environment-jsdom",
   runner: "groups",
+  // Integration tests must be run serially
+  maxWorkers: 1,
   moduleNameMapper: {
     "@/(.*)": "<rootDir>/src/$1",
   },
