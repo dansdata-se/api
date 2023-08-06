99360d8699f5401954b43e432e39e3b32f49e5a6
Felix Zedén Yverås <felix@fzy.se>
2023-08-06 16:39:43 +0000

    feat: dynamically respond with the client's origin in CORS headers
    Required to allow clients running on other origins to access endpoints
    which require authorization.
    
    Based on https://vercel.com/guides/how-to-enable-cors

--- src/api/index.ts
+++ src/api/index.ts
@@ -35,45 +35,47 @@ export function defineEndpoints(
 }
 
 const buildRequestHandler = (endpoints: Partial<Endpoints>) =>
-  withResponseLogger(async (req: NextApiRequest, res: NextApiResponse) => {
-    // Fallback if no endpoints are registered
-    if (Object.keys(endpoints).length === 0) {
-      res.status(404).json({
-        code: ErrorCode.notFound,
-        message: "The requested resource does not exist.",
-      });
-      return;
-    }
+  withResponseLogger(
+    withCorsHeaders(async (req: NextApiRequest, res: NextApiResponse) => {
+      // Fallback if no endpoints are registered
+      if (Object.keys(endpoints).length === 0) {
+        res.status(404).json({
+          code: ErrorCode.notFound,
+          message: "The requested resource does not exist.",
+        });
+        return;
+      }
 
-    const endpoint = endpoints[req.method as keyof typeof endpoints] ?? null;
+      const endpoint = endpoints[req.method as keyof typeof endpoints] ?? null;
 
-    if (endpoint) {
-      const handler = wrapEndpointWithMiddleware(endpoint, endpoints);
-      await handler(req, res);
-    } else if (req.method === "OPTIONS") {
-      res.setHeader(
-        "Allow",
-        Object.entries({
-          OPTIONS: true,
-          HEAD: endpoints.HEAD !== undefined,
-          GET: endpoints.GET !== undefined,
-          POST: endpoints.POST !== undefined,
-          PUT: endpoints.PUT !== undefined,
-          DELETE: endpoints.DELETE !== undefined,
-          PATCH: endpoints.PATCH !== undefined,
-        })
-          .filter(([, isAllowed]) => isAllowed)
-          .map(([header]) => header)
-          .join(", ")
-      );
-      res.status(204).end();
-    } else {
-      (res as NextApiResponse<ErrorDTO>).status(405).json({
-        code: ErrorCode.httpMethodNotAllowed,
-        message: "HTTP method not allowed.",
-      });
-    }
-  });
+      if (endpoint) {
+        const handler = wrapEndpointWithMiddleware(endpoint, endpoints);
+        await handler(req, res);
+      } else if (req.method === "OPTIONS") {
+        res.setHeader(
+          "Allow",
+          Object.entries({
+            OPTIONS: true,
+            HEAD: endpoints.HEAD !== undefined,
+            GET: endpoints.GET !== undefined,
+            POST: endpoints.POST !== undefined,
+            PUT: endpoints.PUT !== undefined,
+            DELETE: endpoints.DELETE !== undefined,
+            PATCH: endpoints.PATCH !== undefined,
+          })
+            .filter(([, isAllowed]) => isAllowed)
+            .map(([header]) => header)
+            .join(", ")
+        );
+        res.status(204).end();
+      } else {
+        (res as NextApiResponse<ErrorDTO>).status(405).json({
+          code: ErrorCode.httpMethodNotAllowed,
+          message: "HTTP method not allowed.",
+        });
+      }
+    })
+  );
 
 /**
  * Logs requests and the responses they produce
@@ -95,4 +97,24 @@ const withResponseLogger =
         status: res.statusCode,
       },
     });
+  };
+
+/**
+ * Writes CORS headers for the request before invoking the given handler.
+ */
+const withCorsHeaders =
+  (handler: ApiRequestHandler): ApiRequestHandler =>
+  async (req, res) => {
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Origin", req.headers.origin ?? "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,HEAD,OPTIONS,PATCH,DELETE,POST,PUT"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+    );
+
+    await handler(req, res);
   };
