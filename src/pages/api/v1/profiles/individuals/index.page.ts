f7e41b8cb6332f530e74d3a6c29d7fd1f26a30f1
Felix Zedén Yverås <felix@fzy.se>
2023-08-27 17:53:31 +0000

    feat: add unimplemented endpoints for creating individuals and organizations

---
+++ src/pages/api/v1/profiles/individuals/index.page.ts
@@ -0,0 +1,53 @@
+import { defineEndpoints } from "@/api";
+import { placeholderAuth } from "@/api/auth";
+import { ErrorCode } from "@/api/dto/error";
+import { CreateIndividualDtoSchema } from "@/api/dto/profiles/individuals/create_individual";
+import { withParsedObject } from "@/api/util";
+import z from "@/api/zod";
+import { NextApiResponse } from "next";
+
+export default defineEndpoints({
+  POST: {
+    authenticated: true,
+    docs: {
+      method: "post",
+      path: "/api/v1/profiles/individuals/",
+      tags: ["Profiles", "Individuals"],
+      security: [{ [placeholderAuth.name]: [] }],
+      summary: "Create a profile for an individual",
+      request: {
+        body: {
+          required: true,
+          content: {
+            "application/json": {
+              schema: CreateIndividualDtoSchema,
+            },
+          },
+        },
+      },
+      responses: {
+        "201": {
+          description: "Created",
+          content: {
+            "application/json": {
+              schema: z.string().cuid(),
+            },
+          },
+        },
+      },
+    },
+    async handler(req, res) {
+      await withParsedObject(
+        CreateIndividualDtoSchema,
+        req.body,
+        res,
+        ErrorCode.invalidBody,
+        // TODO(FelixZY): Implement
+        () => {
+          (res as NextApiResponse<void>).status(501).end();
+          return Promise.resolve();
+        }
+      );
+    },
+  },
+});
---
+++ src/pages/api/v1/profiles/organizations/index.page.ts
@@ -0,0 +1,53 @@
+import { defineEndpoints } from "@/api";
+import { placeholderAuth } from "@/api/auth";
+import { ErrorCode } from "@/api/dto/error";
+import { CreateOrganizationDtoSchema } from "@/api/dto/profiles/organizations/create_organization";
+import { withParsedObject } from "@/api/util";
+import z from "@/api/zod";
+import { NextApiResponse } from "next";
+
+export default defineEndpoints({
+  POST: {
+    authenticated: true,
+    docs: {
+      method: "post",
+      path: "/api/v1/profiles/organizations/",
+      tags: ["Profiles", "Organizations"],
+      security: [{ [placeholderAuth.name]: [] }],
+      summary: "Create a profile for an organization",
+      request: {
+        body: {
+          required: true,
+          content: {
+            "application/json": {
+              schema: CreateOrganizationDtoSchema,
+            },
+          },
+        },
+      },
+      responses: {
+        "201": {
+          description: "Created",
+          content: {
+            "application/json": {
+              schema: z.string().cuid(),
+            },
+          },
+        },
+      },
+    },
+    async handler(req, res) {
+      await withParsedObject(
+        CreateOrganizationDtoSchema,
+        req.body,
+        res,
+        ErrorCode.invalidBody,
+        // TODO(FelixZY): Implement
+        () => {
+          (res as NextApiResponse<void>).status(501).end();
+          return Promise.resolve();
+        }
+      );
+    },
+  },
+});
