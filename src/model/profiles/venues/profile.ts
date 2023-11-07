925f9eb7c1cfd5cfe93d15d90f61c38fe697c157
Felix Zedén Yverås <felix@fzy.se>
2023-11-07 21:54:25 +0000

    docs: add documentation to the `ancestors` and `children` fields of VenueModel

--- src/model/profiles/venues/profile.ts
+++ src/model/profiles/venues/profile.ts
@@ -10,7 +10,17 @@ export interface VenueModel extends BaseProfileModel {
   type: typeof ProfileType.venue;
   coords: CoordsModel;
   permanentlyClosed: boolean;
+  /**
+   * _All ancestors_ of this venue, starting from the root ancestor and ending with
+   * the venue's direct parent.
+   */
   ancestors: VenueReferenceModel[];
+  /**
+   * _Direct_ children of this venue.
+   *
+   * Note that this array does not contain children's children as this could
+   * theoretically explode in size (though in practice, that would be most unexpected).
+   */
   children: VenueReferenceModel[];
 }
 
