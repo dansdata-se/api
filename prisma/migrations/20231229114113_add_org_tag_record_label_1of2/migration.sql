5c7e788d40006097a258be5dbfe456cb260fa25f
Felix Zedén Yverås <felix@fzy.se>
2023-12-29 11:49:24 +0000

    feat: add organization tag `record_label`

---
+++ prisma/migrations/20231229114113_add_org_tag_record_label_1of2/migration.sql
@@ -0,0 +1,2 @@
+-- AlterEnum
+ALTER TYPE "profiles"."organization_tag" ADD VALUE 'record_label';
---
+++ prisma/migrations/20231229114709_add_org_tag_record_label_2of2/migration.sql
@@ -0,0 +1,8 @@
+-- Insert Values
+INSERT INTO "profiles"."organization_tag_details"
+VALUES (
+        'record_label',
+        'Skivbolag',
+        -- A company or association that produces and sells music
+        'Företag eller förening som producerar och säljer musik'
+    );
